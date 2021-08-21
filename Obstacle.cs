59a06995c566818a78ffca755b4013c336b1bf55
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-08-21 15:45:17 -0300

    colocando as alturas de cada prefap

--- Obstacle.cs
+++ Obstacle.cs
@@ -4,33 +4,74 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public Rigidbody rb;
     public float speed;
-    public bool CanAccelerate;
+    public float positionY;
     // Start is called before the first frame update
     void Start()
     {
-        CanAccelerate = false;
-        gameObject.AddComponent<Rigidbody>();
+        GetTheGameObjectName();
+        gameObject.transform.position = new Vector3(0, positionY, 0);
+
         gameObject.AddComponent<BoxCollider>();
-        for (int x = 0; x < gameObject.GetComponentsInChildren<Transform>().Length - 1; x++)
-        {
-            gameObject.transform.GetChild(x).gameObject.AddComponent<BoxCollider>();
-        }
         speed = Random.Range(8, 11);
-        rb = GetComponent<Rigidbody>();
+        //transform.rotation = Quaternion.Euler(0, 180, 55);
+        //gameObject.transform.Rotate(0, 0, rotateForce * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CanAccelerate == true)
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        WheelsRotationAnimation();
+    }
+    public void WheelsRotationAnimation()
+    {
+        for (int x = 0; x < gameObject.GetComponentsInChildren<Transform>().Length - 1; x++)
         {
-            rb.velocity = Vector3.forward * speed;
+            gameObject.transform.GetChild(x).gameObject.transform.Rotate(0, 0, (-speed * 10) * Time.deltaTime);
         }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        CanAccelerate = true;
+    public void GetTheGameObjectName()
+    {       
+        if (gameObject.name.Contains("Car_1"))
+        {
+            positionY = 0.86f;
+        }
+        if (gameObject.name.Contains("Car_2"))
+        {
+            positionY = 0.77f;
+        }
+        if (gameObject.name.Contains("Car_3"))
+        {
+            positionY = 0.8f;
+        }        
+        if (gameObject.name.Contains("Car_4"))
+        {
+            positionY = 1.273f;
+        }        
+        if (gameObject.name.Contains("Car_5"))
+        {
+            positionY = 1f;
+        }        
+        if (gameObject.name.Contains("Car_6"))
+        {
+            positionY = 0.87f;
+        }
+        if (gameObject.name.Contains("Bus"))
+        {
+            positionY = 1.64f;
+        }
+        if (gameObject.name.Contains("Police"))
+        {
+            positionY = 0.8f;
+        }        
+        if (gameObject.name.Contains("Truck_1"))
+        {
+            positionY = 2f;
+        }        
+        if (gameObject.name.Contains("Truck_2"))
+        {
+            positionY = 1.8f;
+        }
     }
 }
