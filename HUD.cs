1617c20b38851e93780905c0c393dd8d679c96cd
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-09-04 16:59:18 -0300

    colocando botão de pause (FUNCIONANDO)

--- HUD.cs
+++ HUD.cs
@@ -8,9 +8,41 @@ public class HUD : MonoBehaviour
     public Text CoinsPoint;
     private int coins;
 
+    public void CleanHUD()
+    {
+        for (int i = 1; i < gameObject.gameObject.GetComponentsInChildren<Transform>().Length; i++)//clean the HUD
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+    private void SetCurrentScene(string scene)
+    {
+        CleanHUD();
+        if (scene == "Gameplay")
+        {
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        if (scene == "Pause")//activated the store
+        {
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+        }
+    }
     public void IncreaseCoins()
     {
         coins += 1;
         CoinsPoint.text = coins.ToString();
+    }
+    public void PauseButton()
+    {
+        if (Time.timeScale == 1)//pause
+        {
+            Time.timeScale = 0;
+            SetCurrentScene("Pause");
+        }        
+        else if (Time.timeScale == 0)//unpause
+        {
+            Time.timeScale = 1;
+            SetCurrentScene("Gameplay");
+        }
     }
 }
